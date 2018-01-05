dff5a485b03eb26e1c6dcc207688a30e87a0ec9e
Luiz <luizrodrigo46@hotmail.com>
2018-01-05 20:02:43 -0300

    first implementation of MipsInstruction class

--- core.py
+++ core.py
@@ -1,3 +1,26 @@
+class MipsInstruction:
+    op = None
+    rs = None
+    rt = None
+    rd = None
+    shamt = None
+    func = None
+    offset = None
+    instruction_type = None
+
+    def __init__(self, instruction):
+        pass
+
+    def has_offset(self):
+        pass
+
+    def get_type(self):
+        return self.instruction_type
+
+    def _check_instruction(self):
+        pass
+
+
 class PC:
     def __init__(self, filename="instructions_file.txt"):
         self.file = open(filename, 'r')
