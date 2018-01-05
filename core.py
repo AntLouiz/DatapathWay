818746360f385c53a7844e06c7facdf4c1285433
Luiz <luizrodrigo46@hotmail.com>
2018-01-05 19:59:49 -0300

    PC class added to core.py

---
+++ core.py
@@ -0,0 +1,18 @@
+class PC:
+    def __init__(self, filename="instructions_file.txt"):
+        self.file = open(filename, 'r')
+        self.next_instruction = None
+
+    def get_instructions(self):
+        """
+        Return a mips instruction object
+        for each instruction in the file
+        """
+
+        for instruction in self.file.readlines():
+            if self.next_instruction:
+                self.next_instruction = MipsInstruction(instruction)
+            else:
+                self.next_instruction = MipsInstruction(instruction)
+
+            yield self.next_instruction
