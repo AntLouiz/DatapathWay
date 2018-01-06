98c9693981086b9d5122ea50a1bbd7a55ec73b0d
Luiz <luizrodrigo46@hotmail.com>
2018-01-06 01:52:18 -0300

    first ALU implementation

---
+++ logic.py
@@ -0,0 +1,37 @@
+from utils import extend_to_32, to_binary
+
+
+class ALU:
+    def makeSum(self, a, b):
+
+        result = to_binary((int(a, 2) + int(b, 2)))
+        return extend_to_32(result)
+
+    def makeSub(self, a, b):
+        negative = False
+
+        result = int(a, 2) - int(b, 2)
+
+        if result < 0:
+            negative = True
+
+        result = to_binary(abs(result))
+
+        return extend_to_32(result, negative)
+
+    def makeAnd(self, a, b):
+
+        a = int(a, 2)
+        b = int(b, 2)
+
+        result = to_binary((a & b))
+
+        return extend_to_32(result)
+
+    def makeOr(self, a, b):
+        a = int(a, 2)
+        b = int(b, 2)
+
+        result = to_binary((a | b))
+
+        return extend_to_32(result)
