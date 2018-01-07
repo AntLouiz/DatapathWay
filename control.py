c8d5dc51dd7f360c52ceac630079b97c16c94eaf
Luiz <luizrodrigo46@hotmail.com>
2018-01-07 17:59:08 -0300

    some ajusts on offset and the alu result

--- control.py
+++ control.py
@@ -157,7 +157,7 @@ class ControlLw(BaseControl):
     def execute(self):
         instruction = self.cpu.pc.next_instruction
         registers = instruction.get_registers()
-        offset = instruction.offset
+        offset = to_binary(int(instruction.offset, 2))
         print(instruction)
 
         rt = registers['rt']
@@ -171,6 +171,7 @@ class ControlLw(BaseControl):
         print("ALU-in-2: {}".format(offset))
 
         alu_result = self.cpu.alu.makeSum(register_data, offset)
+        alu_result = to_binary(int(alu_result, 2))
         print("ALU-result: {}".format(alu_result))
 
         print("Address: {}".format(alu_result))
@@ -190,7 +191,7 @@ class ControlSw(BaseControl):
     def execute(self):
         instruction = self.cpu.pc.next_instruction
         registers = instruction.get_registers()
-        offset = instruction.offset
+        offset = to_binary(int(instruction.offset, 2))
         print(instruction)
 
         rs = registers['rs']
