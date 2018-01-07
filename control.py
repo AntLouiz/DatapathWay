dff43a91b85e9b854a7f8b8bf9feb3a9ac34d262
Luiz <luizrodrigo46@hotmail.com>
2018-01-07 00:53:19 -0300

    ControlSub strategy added

--- control.py
+++ control.py
@@ -18,3 +18,11 @@ class ControlAdd(BaseControl):
         print("Leio o registrador 2")
         print("Calculo o valor dos registradores na ULA")
         print("Retorno para o registrador de escrita setado na instruçao")
+
+class ControlSub(BaseControl):
+
+    def execute(self):
+        print("Leio o registrador 1")
+        print("Leio o registrador 2")
+        print("Subtraio o valor dos registradores na ULA")
+        print("Retorno para o registrador de escrita setado na instruçao")
