32cce251310fcc3255b1c23aad4e844bbeb472e2
Luiz <luizrodrigo46@hotmail.com>
2018-01-07 00:57:08 -0300

    ControlAnd added

--- control.py
+++ control.py
@@ -26,3 +26,12 @@ class ControlSub(BaseControl):
         print("Read the register 2")
         print("Sub the registers values on the ALU")
         print("Return the result to the write register")
+
+
+class ControlAnd(BaseControl):
+
+    def execute(self):
+        print("Read the register 1")
+        print("Read the register 2")
+        print("Make a AND with the registers values on the ALU")
+        print("Return the result to the write register")
