783135628400635ae4d9d052def1ec3f5adff20c
Luiz <luizrodrigo46@hotmail.com>
2018-01-15 09:47:00 -0300

    a new decorator added to consider the binary number signal

--- logic.py
+++ logic.py
@@ -2,11 +2,21 @@ from utils import extend_to_32, to_binary
 
 
 class ALU:
+
+    @sinalize
     def makeSum(self, a, b):
+        negative = False
 
-        result = to_binary((int(a, 2) + int(b, 2)))
-        return extend_to_32(result)
+        result = int(a, 2) + int(b, 2)
 
+        if result < 0:
+            negative = True
+
+        result = to_binary(abs(result))
+
+        return extend_to_32(result, negative)
+
+    @sinalize
     def makeSub(self, a, b):
         negative = False
 
